4fe4a8d0849b048d2230b4c28c8ec05712c691dd
dimas <my@gmail.com>
2021-01-21 16:49:56 +0200

    add layouts

---
+++ resources/views/pages/allusers.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.index')
+
+@section('content')
+  <h1>All Users</h1>
+@endsection
---
+++ resources/views/pages/home.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.index')
+
+@section('content')
+  <div class="container">
+    <div class="ro">
+      <div class="col">
+        <h1>Welcome</h1>
+        <h2>Test task for trainee completed on laravel by Olvach Dmitriy</h2>
+      </div>
+    </div>
+  </div>
+@endsection
